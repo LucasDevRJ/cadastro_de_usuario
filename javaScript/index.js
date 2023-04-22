88e04315286b83a5c3073238504bc889ddb3f9bc
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-22 17:06:47 -0300

    Criação de usuario

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,6 +1,8 @@
 function cadastrarUsuario() {
 	var campoNome = document.getElementById("nome");
 	var campoSobrenome = document.getElementById("sobrenome");
+	var campoData = document.getElementById("data");
+	var campoSenha = document.getElementById("senha");
 
 	var generos = document.getElementsByName("genero");
 
@@ -9,4 +11,20 @@ function cadastrarUsuario() {
 			console.log("Gênero = " + generos[i].value);
 		}
 	}
+
+	var nome = campoNome.value;
+	var sobrenome = campoSobrenome.value;
+	var data = campoData.value;
+	var senha = campoSenha.value;
+
+	var usuario = {
+		nome : nome,
+		sobrenome : sobrenome,
+		data : data,
+		genero : genero,
+		senha : senha
+	}
 }
+
+var botao = document.querySelector("button");
+botao.onclick = cadastrarUsuario;
